2c15434cae59174f3483961a8e89ca19df6ccc53
gichiraanne <gichiraanne@gmail.com>
2017-05-12 10:54:23 +0300

    The search functionality for blogs

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .forms import *
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from datetime import datetime
+from django.db.models import Q
 
 
 def blog(request,id):
@@ -21,12 +22,37 @@ def blog(request,id):
         recent_post =  BlogPost.objects.all().order_by("date_created").latest("date_created")
     else:
         recent_post =  BlogPost.objects.get(id=id)
-        
+
+    # most recent posts
+    blog_post = BlogPost.objects.all().order_by("date_created")
+
+    # search blogs
+    key_word = request.GET.get('search', None)
+
+    if key_word:
+        query_list = key_word.split()
+        for q in query_list:
+
+            qset = (
+                Q(id__icontains=q) |
+                Q(title__icontains=q) |
+                Q(details__icontains=q) |
+                Q(created_by__icontains=q) |
+                Q(date_created__icontains=q) 
+            )
+
+            blog_post = blog_post.filter(qset)
+    # end search        
+
+    querydict = request.GET.copy()
 
     context = {
         "title": "Blog",
+        "query_string": querydict.urlencode(),
+        "query": request.GET.get('search'),
+        "num_blog": len(blog_post),
         "recent_post":recent_post,
-        "blog_post": BlogPost.objects.all().order_by("date_created"),
+        "blog_post": blog_post,
 
     } 
 
