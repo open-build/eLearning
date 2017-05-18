5356cd01820e96824eaec8148886ad8fc978331b
gichiraanne <gichiraanne@gmail.com>
2017-05-18 08:12:57 +0300

    Further enhancement of blog functionalities

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
+def attachment_path(filename):
+    # defining the unique path to the blog image
+    import os
+    from django.conf import settings
+    os.umask(0)
+    path = 'blog/attachments/%s'
+    att_path = os.path.join(settings.MEDIA_ROOT, path)
+    if settings.DEFAULT_FILE_STORAGE == "django.core.files.storage.FileSystemStorage":
+        if not os.path.exists(att_path):
+            os.makedirs(att_path, 0o777)
+    return os.path.join(path, filename)
+
 class BlogPost(models.Model):
     title = models.CharField("Title", max_length=300,)
     details = models.TextField("Details", null=True, blank=True)
+    image =  models.ImageField(upload_to="/openbuild/elearning/media/blog/", null=True, blank=True)
     date_created = models.DateTimeField(null=True, blank=True)
     created_by = models.CharField("author", default="OpenBuild Team", max_length=100,)
+
 
 class BlogComment(models.Model):
 	blog_id = models.ForeignKey(BlogPost,related_name=('blog_comments'),)
 	comment = models.CharField("Comment", max_length=500,)
+	created_by = models.CharField("Sender", default="Anonymous", max_length=100,)
+	sender_email = models.EmailField("Sender email ", blank=True, null=True,)
+	posted_date = models.DateTimeField(null=True, blank=True)
+
+ 
+class Response(models.Model):
+	blog_id = models.ForeignKey(BlogPost,related_name=('response'),)
+	comment_id = models.ForeignKey(BlogComment,related_name=('reply'),)
+	reply = models.CharField("Response", max_length=500,)
 	created_by = models.CharField("Sender", default="Anonymous", max_length=100,)
 	sender_email = models.EmailField("Sender email ", blank=True, null=True,)
 	posted_date = models.DateTimeField(null=True, blank=True)
--- blog/views.py
+++ blog/views.py
@@ -6,28 +6,37 @@ from datetime import datetime
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
-
+from PIL import Image
 
 def blog(request,id):
     if request.method == 'POST':
         title = request.POST.get("title")
         details = request.POST.get("details")
+        image = {}
+        if request.FILES:
+            image = request.FILES['image']
+        print "Anne"
+        print image
         created_by = request.user
         date_created = datetime.now()
-        post = BlogPost(title=title, details=details, date_created=date_created, created_by = created_by)
-        post.save() 
+        post = BlogPost(title=title, details=details, image=image, date_created=date_created, created_by = created_by)
+        post.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))  
     
     # latest posts
-    blog_post = BlogPost.objects.all().prefetch_related('blog_comments').order_by("date_created")
-
+    blog_post = BlogPost.objects.all().prefetch_related('blog_comments','response').order_by("date_created")
+    
     recent_post = {}
     if int(id) == 0:
         recent_post =  blog_post.latest("date_created")
     else:
         recent_post =  blog_post.get(id=id)
 
-
+    # print "Anne"
+    # print image 
+    # show_image = Image.open(image)
     # search blogs
+    # image = recent_post.image
     key_word = request.GET.get('search', None)
 
     if key_word:
@@ -44,9 +53,6 @@ def blog(request,id):
 
             blog_post = blog_post.filter(qset)
     # end search  
-
-    #comment
-    comments =  BlogComment.objects.all()    
 
     querydict = request.GET.copy()
 
@@ -73,4 +79,19 @@ def post_comment(request,blog_id):
         posted_date = datetime.now()
         comment = BlogComment(blog_id=blog_id, comment=comment, created_by=created_by, sender_email=sender_email,posted_date=posted_date)
         comment.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER')) 
+
+def comment_reply(request,blog_id,comment_id):
+    blog = get_object_or_404(BlogPost, id=blog_id)
+    comment = get_object_or_404(BlogComment, id=comment_id)
+
+    if request.method == 'POST':
+        blog_id = blog
+        comment_id = comment
+        reply = request.POST.get("reply")
+        created_by = request.POST.get("name")
+        sender_email = request.POST.get("email")
+        posted_date = datetime.now()
+        reply = Response(blog_id=blog_id, comment_id=comment_id, reply=reply, created_by=created_by, sender_email=sender_email,posted_date=posted_date)
+        reply.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER')) 
